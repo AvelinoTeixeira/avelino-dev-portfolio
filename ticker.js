ab6d8b52bc9e23775adc6245f0937ab6bc312c75
Avelino Teixeira <137221452+AvelinoTeixeira@users.noreply.github.com>
2025-02-01 17:12:54 +0100

    slider infinit

---
+++ ticker.js
@@ -0,0 +1,9 @@
+// Este código é para garantir que as imagens se repitam infinitamente
+const slider = document.querySelector('.image-slider');
+
+// Clone os elementos para que o movimento pareça infinito
+const images = document.querySelectorAll('.image-slider img');
+images.forEach(image => {
+  const clone = image.cloneNode(true);
+  slider.appendChild(clone);
+});
